8b8934a5daea1848affa7ca327b67b0e18c94255
Mrinmay12 <88670421+Mrinmay12@users.noreply.github.com>
2024-05-12 19:40:17 +0530

    Add files via upload

--- Utils.js
+++ Utils.js
@@ -1,12 +1,31 @@
 import User from "./Users.js"
-
-
-export const Userdetails = async (userIds) => {
+import ProfilePic from "./Profilepic.js";
+ const UserProfilePic = async (userIds,sex) => {
+    let user_gander=sex===undefined?"1":sex
     try {
-        const users = await User.find({ message_id: { $in: userIds } });
+        const user = await ProfilePic.findOne({ user_id: userIds });
+        if (!user) {
+            let profile_img =user_gander==="1"? 'https://firebasestorage.googleapis.com/v0/b/voter-29270.appspot.com/o/boypic.jpg?alt=media&token=6d9dc8a7-8c16-48d6-8df2-8e37c6cef89b':'https://firebasestorage.googleapis.com/v0/b/voter-29270.appspot.com/o/girlpic.jpg?alt=media&token=9227e456-c6ea-414f-87a0-991bce8b81a2'
+            return profile_img
+          }else{
+           return  user.profile_img 
+          }
+
+    } catch (error) {
+        console.error(`Error fetching user data: ${error}`);
+        return {};
+    }
+};
+export const Userdetails = async (userIds,socketId,location) => {
+    try {
+        const users = await User.find({ message_id:  userIds  });
+        // console.log(users);
+        const profilepic=await UserProfilePic(users[0]?.user_id,users[0]?.sex)
+        // console.log(profilepic);
         const userMap = {};
         users.forEach((user) => {
-            userMap[user.message_id] = { name: user.name, sex: user.sex,message_id:user.message_id,user_id:user.user_id };
+            userMap[user.message_id] = { name: user.name, socketId:socketId,location:location,user_id:user._id,work_title:user.work_title||"",user_image:profilepic,message_id:  userIds 
+         };
         });
         return userMap;
     } catch (error) {
--- index.js
+++ index.js
@@ -44,35 +44,79 @@ const io = new Server(socketport, {
 });
 
 global.onlineUsers = new Map();
+global.nearUsers = new Map();
 let activeUsers=[]
+let near_users = [];
+
+const calculateDistance = (coord1, coord2) => {
+  const R = 6371; // Radius of the Earth in km
+  const dLat = (coord2.latitude - coord1.latitude) * (Math.PI / 180);
+  const dLon = (coord2.longitude - coord1.longitude) * (Math.PI / 180);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(coord1.latitude * (Math.PI / 180)) *
+      Math.cos(coord2.latitude * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+  return distance; // Distance in km
+};
 
 io.on("connection", (socket) => {
   global.chatSocket = socket;
   socket.on("add-user", (userId) => {
-    console.log(userId)
     onlineUsers.set(userId, socket.id);
-    if (userId !== "") { // Check if userId is not empty
-        if (!activeUsers.some((user) => user.userId === userId)) {
-            activeUsers.push({ userId: userId, socketId: socket.id });
-        }
-    }
-    io.emit("get-users", activeUsers);
-});
+    if(!activeUsers.some((user)=>user.userId===userId)){
 
+      activeUsers.push({userId:userId,socketId:socket.id})
+    }
+    io.emit("get-users",activeUsers)
+  });
+
+
+ //near user function get 
+  socket.on("userLocation", async(location) => {
+    let cc = await Userdetails(location.user_id,socket.id,location);
+  let Modified_data; 
+  if(location?.user_id){
+    Modified_data=cc[location?.user_id]
+  }
+ 
+    nearUsers.set(location, socket.id);
+    if(typeof Modified_data === "object"){
+    if(!near_users.some((user)=>user.user_id===Modified_data.user_id)){
+      
+      near_users.unshift(Modified_data)
+      }
+
+  }
+  // console.log(near_users);
+
+    // Calculate distance between current user and other users
+    const nearUsersdata = near_users.filter((user) => {
+if(Object.keys(location).length !== 0){
+      const distance = calculateDistance(user.location, location);
+      return distance <2; 
+}
+    });
+
+// console.log(nearUsersdata);
+    // Emit near users back to the client
+    io.emit("nearUsers", nearUsersdata);
+  });
 
 socket.on("disconnect",()=>{
   activeUsers=activeUsers.filter((user)=>user.socketId !==socket.id)
   io.emit("get-users",activeUsers)
+  near_users=near_users.filter((user)=>user.socketId !==socket.id)
+  io.emit("nearUsers", near_users);
 })
 
-socket.on("send-msg", (data) => {
-  const sendUserSocket = onlineUsers.get(data.to);
-  if (sendUserSocket) {
-      console.log(data, data.to, sendUserSocket);
-      io.to(sendUserSocket).emit("msg-recieve", data.messagetext, data.messageid);
-  } else {
-      console.log(`User with ID ${data.to} is not online.`);
-      // Handle the case where the user is not online
-  }
-});
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.messagetext,data.messageid);
+    }
+  });
 });
